6d1af2aedaa612657f68b06673e26c0e039b4ec6
goldenfox12 <115833831+goldenfox12@users.noreply.github.com>
2023-06-04 20:33:06 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function setup(){
+    canvas = creatCanvas(300, 300);
+    canvas.center();
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
+createCanvas(width,height)
+{
+    height:30px;
+    width:30px;
+}
